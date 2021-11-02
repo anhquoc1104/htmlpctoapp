529cc0bbf05504ad11143cc04f665fc59b920bce
quocpa1104 <quocpa1@tinhvan.com>
2021-11-02 21:30:38 +0700

    fix

--- script.js
+++ script.js
@@ -17,3 +17,57 @@ $('.location-header-text img').addEventListener('click', function () {
     $('.root').style.display = 'flex'
     $('.root-province').style.display = 'none'
 })
+
+$('.province').addEventListener('click', function (e) {
+    const province = e.target.id
+    console.log(province);
+
+    switch (province) {
+        case 'khanhhoa':
+            $('.qr-code img').src = "./resources/img/qr/qr-code-khanhhoa.jpg"
+            $('.header-location span').innerHTML = 'Khánh Hòa'
+            break
+        case 'hochiminh':
+            $('.qr-code img').src = "./resources/img/qr/qr-code-hochiminh.png"
+            $('.header-location span').innerHTML = 'Hồ Chí Minh'
+            break
+        case 'dongnai':
+            $('.qr-code img').src = "./resources/img/qr/qr-code-dongnai.jpg"
+            $('.header-location span').innerHTML = 'Đồng Nai'
+            break
+        case 'ninhthuan':
+            $('.qr-code img').src = "./resources/img/qr/qr-code-ninhthuan.jpg"
+            $('.header-location span').innerHTML = 'Ninh Thuận'
+            break
+        case 'binhthuan':
+            $('.qr-code img').src = "./resources/img/qr/qr-code-binhthuan.jpg"
+            $('.header-location span').innerHTML = 'Bình Thuận'
+            break
+        case 'binhduong':
+            $('.qr-code img').src = "./resources/img/qr/qr-code-binhduong.jpg"
+            $('.header-location span').innerHTML = 'Bình Dương'
+            break
+        case 'binhphuoc':
+            $('.qr-code img').src = "./resources/img/qr/qr-code-binhphuoc.jpg"
+            $('.header-location span').innerHTML = 'Bình Phước'
+            break
+        case 'cantho':
+            $('.qr-code img').src = "./resources/img/qr/qr-code-cantho.jpg"
+            $('.header-location span').innerHTML = 'Cần Thơ'
+            break
+        case 'danang':
+            $('.qr-code img').src = "./resources/img/qr/qr-code-danang.jpg"
+            $('.header-location span').innerHTML = 'Đà Nẵng'
+            break
+        case 'daklak':
+            $('.qr-code img').src = "./resources/img/qr/qr-code-daklak.jpg"
+            $('.header-location span').innerHTML = 'Đắk Lắk'
+            break
+        default:
+            console.log(province);
+    }
+
+    $('.root').style.display = 'flex'
+    $('.root-province').style.display = 'none'
+    return;
+})
